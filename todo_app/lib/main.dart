2950630905f42025b662adf0026f63973eae79d2
Mario Weidler <>
2020-08-01 11:08:18 +0200

    Add provider package and wrap app into provider

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/state/todo_model.dart';
 
 void main() {
-  runApp(TodoListApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TodoModel(),
+      child: TodoListApp(),
+    ),
+  );
 }
 
 class TodoListApp extends StatelessWidget {
@@ -10,7 +17,9 @@ class TodoListApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo list',
       home: Scaffold(
-        appBar: AppBar(title: Text("Todo list"),),
+        appBar: AppBar(
+          title: Text("Todo list"),
+        ),
       ),
     );
   }
