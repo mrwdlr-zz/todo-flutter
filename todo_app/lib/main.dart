e8e4a9afe9623c72f435c02784839956f3a5c8d6
Mario Weidler <>
2020-08-01 14:21:37 +0200

    Render a TodoList which is connected to the AppState

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -20,7 +20,24 @@ class TodoListApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Todo list"),
         ),
+        body: TodoList(),
       ),
     );
+  }
+}
+
+class TodoList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TodoModel>(
+        builder: (context, todoList, child) => ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: todoList.todos.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                child: Center(child: Text(todoList.todos[index].title)),
+              );
+            }));
   }
 }
